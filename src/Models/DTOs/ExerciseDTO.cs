df3849b1cf184c1b52974849f997772c29e56467
Oleksandr <okarakuzov@gmail.com>
2026-03-24 18:29:21 +0200

    add models (#1)
    Co-authored-by: Oleksandr <oleksandrhelpa@gmail.com>

---
+++ src/Models/DTOs/ExerciseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace src.Models.DTOs
+{
+    public class ExerciseDTO
+    {
+        [Required(ErrorMessage ="Ім'я є обов'язковим")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Кількість підходів є обов'язковим")]
+        [Range(1, 20, ErrorMessage ="Кількість підходів некоректна")]
+        public int CountApproaches { get; set; }
+
+        [Required(ErrorMessage = "Кількість повторень є обов'язковим")]
+        [Range(1, 100, ErrorMessage = "Кількість повторень некоректна")]
+        public int CountRepeats { get; set; }
+    }
+}
---
+++ src/Models/DTOs/WorkoutDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace src.Models.DTOs
+{
+    public class WorkoutDTO
+    {
+        [Required(ErrorMessage = "Ім'я є обов'язковим")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Дата є обов'язковою")]
+        [Range(typeof(DateTime), "1/1/2026", "12/31/2100")]
+        public DateTime Start {  get; set; }
+
+        public List<ExerciseDTO> Exercises { get; set; } = new();
+    }
+}
