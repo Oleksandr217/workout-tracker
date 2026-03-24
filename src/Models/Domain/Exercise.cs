a6ab85b32f1da1558e188f68967904659e51ea57
Oleksandr <oleksandrhelpa@gmail.com>
2026-03-24 18:28:45 +0200

    add models

---
+++ src/Models/Domain/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace src.Models.Domain
+{
+    public class Exercise
+    {
+        [Key]
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public int CountApproaches { get; set; }
+        public int CountRepeats { get; set; }
+        public int WorkoutId { get; set; }
+        public Workout Workout { get; set; }
+    }
+}
---
+++ src/Models/Domain/Workout.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace src.Models.Domain
+{
+    public class Workout
+    {
+        [Key]
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public DateTime Start { get; set; }
+        public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+    }
+}
