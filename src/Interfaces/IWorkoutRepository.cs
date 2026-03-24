1087f3f8b5e79ceb395e93da824a4459dcc0011e
Oleksandr <okarakuzov@gmail.com>
2026-03-24 19:18:29 +0200

    add interfaces (#2)
    Co-authored-by: Oleksandr <oleksandrhelpa@gmail.com>

---
+++ src/Interfaces/IWorkoutRepository.cs
@@ -0,0 +1,16 @@
+﻿using src.Models.Domain;
+
+namespace src.Interfaces
+{
+    public interface IWorkoutRepository
+    {
+        Task<bool> Create(Workout workout);
+        Task<bool> AddExercises(List<Exercise> exercise, int workoutId);
+        Task<bool> DeleteExercise(int workoutId, int exerciseId);
+        Task<bool> UpdateDate(int workoutId, DateTime date);
+        Task<bool> Delete(int id);
+        Task<List<Workout>> GetAll();
+        Task<Workout> GetById(int id);
+        Task<List<Workout>> GetByDate(DateTime date);
+    }
+}
