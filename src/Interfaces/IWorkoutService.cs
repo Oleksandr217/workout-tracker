3e5a97be697ac2631b5e4a66d2b12ae615a241d5
Oleksandr <oleksandrhelpa@gmail.com>
2026-03-24 19:15:32 +0200

    add interfaces

---
+++ src/Interfaces/IWorkoutService.cs
@@ -0,0 +1,17 @@
+﻿using src.Models.Domain;
+using src.Models.DTOs;
+
+namespace src.Interfaces
+{
+    public interface IWorkoutService
+    {
+        Task<bool> Create(WorkoutDTO workout);
+        Task<bool> AddExercises(List<ExerciseDTO> exercise, int workoutId);
+        Task<bool> DeleteExercise(int workoutId, int exerciseId);
+        Task<bool> UpdateDate(int workoutId, DateTime date);
+        Task<bool> Delete(int id);
+        Task<List<WorkoutDTO>> GetAll();
+        Task<WorkoutDTO> GetById(int id);
+        Task<List<WorkoutDTO>> GetByDate(DateTime date);
+    }
+}
