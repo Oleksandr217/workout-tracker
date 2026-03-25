63be690823b551b628b9b7290344cb2b73e04b5a
Oleksandr <oleksandrhelpa@gmail.com>
2026-03-25 17:24:27 +0200

    add repository

---
+++ src/Repositories/WorkoutRepository.cs
@@ -0,0 +1,50 @@
+﻿using src.Interfaces;
+using src.Models.Domain;
+
+namespace src.Repositories
+{
+    public class WorkoutRepository : IWorkoutRepository
+    {
+        
+
+        public async Task<bool> AddExercises(List<Exercise> exercise, int workoutId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Create(Workout workout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Delete(int id)
+        { 
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteExercise(int workoutId, int exerciseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Workout>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Workout>> GetByDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Workout> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateDate(int workoutId, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
